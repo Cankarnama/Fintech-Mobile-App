afd18b90332cc3fbbb139ac8cbc9d66f3cc18371
Your Name <you@example.com>
2022-04-07 20:59:40 +0000

    first commit

---
+++ lib/screen1.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+
+class Screen1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold(
+        body: SafeArea(
+
+          child:
+          Container(
+
+
+
+
+              color: Colors.
+
+
+
+          ),
+        ),
+
+
+
+
+      );
+  }
+}
